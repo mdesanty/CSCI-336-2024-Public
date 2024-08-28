d3ab4843dee040012498b620faaa22eac908cf7a
Michael DeSanty <michael.desanty@gmail.com>
2024-08-28 11:54:06 -0400

    Pun Lab Redo Starter

---
+++ labs/react/pun_redo/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Pun from './components/pun';
+
+function App() {
+  return (
+    <div>
+      <Pun header="I've got a pun for you" />
+    </div>
+  );
+}
+
+export default App;
---
+++ labs/react/pun_redo/src/components/pun.jsx
@@ -0,0 +1,10 @@
+function Pun(props) {
+  return (
+    <div id="pun-container">
+      <h2>{ props.header }</h2>
+      <p id="pun">Blah blah blah</p>
+    </div>
+  );
+}
+
+export default Pun;
