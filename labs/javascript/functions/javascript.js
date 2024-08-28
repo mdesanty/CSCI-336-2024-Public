8ffb690835bdcbcda122f86ec527404fae5dab92
Michael DeSanty <michael.desanty@gmail.com>
2024-08-27 23:07:14 -0400

    JS Functions Lab Starter

---
+++ labs/javascript/functions/javascript.js
@@ -0,0 +1,8 @@
+console.log("But there is something to see here...");
+
+const numbers = [4, 3, -2, 0, 1, -5, -1, 6];
+
+// Maybe write your function here.
+
+// Maybe call your function here.
+// Maybe output the results here with console.log.
